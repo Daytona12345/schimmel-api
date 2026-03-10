5e259a901c850aafdc1ffe131f3c4eba57340cea
Daytona12345 <133211554+Daytona12345@users.noreply.github.com>
2026-03-10 10:16:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from rechner import schimmel_analyse_kern
 
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"message": "Schimmel API läuft"}
+
+
+class AnalyseInput(BaseModel):
+    t_raum: float
+    rF_raum: float
+    t_wand: float
+
+
+@app.post("/calculate")
+def calculate(data: AnalyseInput):
+
+    result = schimmel_analyse_kern(
+        data.t_raum,
+        data.rF_raum,
+        data.t_wand
+    )
+
+    return result
