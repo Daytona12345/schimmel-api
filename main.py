6d2cf512ea4144dc17cae2acd229081a449e375f
Daytona12345 <133211554+Daytona12345@users.noreply.github.com>
2026-03-11 21:02:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from rechner import schimmel_analyse_kern
+from textbausteine import get_textbaustein
 
 app = FastAPI()
 
