dc53647aa6d7cf84f1d83bbee71c4bc58df5812e
Daytona12345 <133211554+Daytona12345@users.noreply.github.com>
2026-03-11 17:36:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,14 +30,38 @@ class AnalyseInput(BaseModel):
     t_wand: float | None = None
     punkte: list[Punkt] | None = None
 
-
 @app.post("/calculate")
 def calculate(data: AnalyseInput):
 
-    result = schimmel_analyse_kern(
-        data.t_raum,
-        data.rF_raum,
-        data.t_wand
-    )
+    # Fall 1: klassische Einzelmessung
+    if data.t_wand is not None:
+        result = schimmel_analyse_kern(
+            data.t_raum,
+            data.rF_raum,
+            data.t_wand
+        )
+        return result
 
-    return result
+    # Fall 2: Mehrpunktmessung (UI 4.1)
+    if data.punkte:
+
+        results = []
+
+        for p in data.punkte:
+            r = schimmel_analyse_kern(
+                data.t_raum,
+                data.rF_raum,
+                p.temp
+            )
+
+            results.append({
+                "punkt": p.name,
+                "temp": p.temp,
+                "analyse": r
+            })
+
+        return {
+            "punkte": results
+        }
+
+    return {"error": "Keine Messwerte übergeben"}
